6208803dac41c770b47e30ef4862d111c577d785
coderdal <agircoder@gmail.com>
2022-07-01 14:04:21 +0300

    First project

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h2>My first Vue JS project</h2>
+  <h1>Counter App</h1>
+
+  <h1 class="count">{{ count }}</h1>
+
+  <button @click="count++">+</button>
+  <button @click="count--" class="down">-</button>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// eslint-disable-next-line
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      count: 0,
+    };
   },
 };
 </script>
@@ -22,5 +29,26 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.count {
+  color: #3a85cf;
+}
+
+button {
+  width: 80px;
+  height: 30px;
+  font-size: 20px;
+  font-weight: 600;
+  background: #3b9b3b;
+  color: #fff;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+  margin: 0 5px;
+}
+
+.down {
+  background: #fc4040;
 }
 </style>
